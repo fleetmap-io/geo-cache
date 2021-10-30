f23bf025853b6c1fbe82b955ee104bdde616e3cd
joaquimcardeira <joaquim_cardeira@hotmail.com>
2021-10-30 23:20:10 +0100

    90 days cache

--- test/curl.sh
+++ test/curl.sh
@@ -1,8 +1,7 @@
 
 set -u -x
 
-  curl 'http://localhost:8851/maps/api/geocode/json' -G --data-urlencode 'address=20 Falstaff Close, Eynsham OX29 4QA'
-  curl 'http://localhost:8851/maps/api/geocode/json' -G --data-urlencode 'address=14 Grays Inn Road, Chancery Lane, London'
+  curl 'http://localhost:8851/maps/api/geocode/json' -G --data-urlencode 'latlng=-23.493394,-46.854586'
   sleep 1
   redis-cli keys cache-geo-cache:*
   redis-cli hkeys cache-geo-cache:url:h
